724bde0b2598a2d2929bc6ce00bee8793cc775d2
piyota0901 <piyo.tatsu.0901@gmail.com>
2023-04-26 23:28:01 +0900

    refactor: change integer to uuid for todo's id

--- todo_app/main.py
+++ todo_app/main.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from uuid import UUID, uuid4
 
 from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse
@@ -8,7 +9,7 @@ app = FastAPI()
 
 
 class Todo(BaseModel):
-    id: int
+    id: UUID
     title: str
     comment: str
     isDone: bool
@@ -21,9 +22,27 @@ class AddTodo(BaseModel):
 
 
 db = [
-    Todo(id=1, title="sample1", comment="sample1です。", isDone=False, create_at="2023-04-25"),
-    Todo(id=2, title="sample2", comment="sample2です。", isDone=False, create_at="2023-04-25"),
-    Todo(id=3, title="sample3", comment="sample3です。", isDone=False, create_at="2023-04-25"),
+    Todo(
+        id=UUID("2ae9a1d9-20b0-4962-9754-d8f6972e425f"),
+        title="sample1",
+        comment="sample1です。",
+        isDone=False,
+        create_at="2023-04-25",
+    ),
+    Todo(
+        id=UUID("d8b45d0a-0419-4798-8f36-2603ef588ab7"),
+        title="sample2",
+        comment="sample2です。",
+        isDone=False,
+        create_at="2023-04-25",
+    ),
+    Todo(
+        id=UUID("d35a6b0c-2ce7-4992-9f73-a5ac2a121797"),
+        title="sample3",
+        comment="sample3です。",
+        isDone=False,
+        create_at="2023-04-25",
+    ),
 ]
 
 
@@ -33,7 +52,7 @@ async def root():
 
 
 @app.get("/todo/{todo_id}")
-async def featch_by_id(todo_id: int) -> Todo:
+async def featch_by_id(todo_id: UUID) -> Todo:
     todo = list(filter(lambda todo: todo.id == todo_id, db))[0]
     return todo
 
@@ -45,9 +64,8 @@ async def fetch_all() -> list[Todo]:
 
 @app.post("/todo/")
 async def add(todo: AddTodo):
-    max_id = max([t.id for t in db])
     create_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    new_todo = Todo(id=max_id + 1, title=todo.title, comment=todo.comment, isDone=False, create_at=create_at)
+    new_todo = Todo(id=uuid4(), title=todo.title, comment=todo.comment, isDone=False, create_at=create_at)
     db.append(new_todo)
     # JSONResponse利用時はシリアライズする必要がある
     # https://fastapi.tiangolo.com/ja/advanced/additional-status-codes/
