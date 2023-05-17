52b63fb58a4638ac21d08e26dec5465cbec69c76
piyota0901 <piyo.tatsu.0901@gmail.com>
2023-05-17 23:31:03 +0900

    chore: add docker for develop

--- backend/db/session.py
+++ backend/db/session.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # https://docs.sqlalchemy.org/en/20/core/pooling.html#disconnect-handling-pessimistic
-engine = create_engine(url="postgresql://todo_app:password@localhost/tododb", pool_pre_ping=True)
+engine = create_engine(url="postgresql://todo_app:password@postgresql15/tododb", pool_pre_ping=True)
 
 # https://docs.sqlalchemy.org/en/20/orm/session_basics.html#using-a-sessionmaker
 SessionLocal = sessionmaker(bind=engine, autoflush=False)
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.routers import todo
+from backend.routers import todo
 
 app = FastAPI()
 
